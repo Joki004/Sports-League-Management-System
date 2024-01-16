5c167fc7ed0c99f728bb1022abfed917981dec1f
Joram Mumb <jorammumb15.jm@gmail.com>
2024-01-17 00:56:45 +0100

    sponsor, fan and sportObject data sample

---
+++ SampleData/FanSampleData.sql
@@ -0,0 +1,10 @@
+INSERT INTO FAN(FAN_ID, FAN_NAME, EMAIL)
+VALUES(1, 
+    'ADAM BIJACIK',
+    'bijacik.adam@gmail.com'
+    );
+INSERT INTO FAN(FAN_ID, FAN_NAME, EMAIL)
+VALUES(2, 
+    'JORAM MUMB MULAJ KAMBAJ',
+    'joram.mumb@gmail.com'
+    );
