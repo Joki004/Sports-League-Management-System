d871dc9bbaf6ef099c5b2080ed4bc98193cb8920
Joram Mumb <jorammumb15.jm@gmail.com>
2024-01-26 08:55:37 +0100

    fixes

--- SQLScripts/InsertSampleData.sql
+++ SQLScripts/InsertSampleData.sql
@@ -111,41 +111,6 @@ CROSS JOIN TABLE(t.players) p;
 
 select * from player;
 
---DODAÆ DANE DO ZAWODNIKÓW - S¥ 3 DRUZYNY Z 15, DODAJ PO 8 LOSOWYCH ZAWODNIKÓW Z RESZTY
---DODAJ PARE SPONSORÓW
---DODAÆ SEKTORY I MIEJSCA DO OBIEKTÓW -> KAZDY OBIEKT MA X SEKTORÓW i Y MIEJSC, KA¯DY SEKTOR MA TYLE SAMO MIEJSC
---JAK DODASZ SEKTORY I MIEJSCA TO PRZERÓBB DODAWANIE OBIEKTÓW SPORTOWYCH - SKRYPT
-
---punkty
---tiebreaker
-
---PRZY ROBIENIU FUNKCJI - PISZMY OD RAZU BLOK PL/SQL KTÓRY TESTUJE TA FUNKCJE ITD.
-
---Funkcja generujaca harmonogram
---Funkcja Dodaj zawodnika -> weryfikacja czy +18
---Funkcja Sprawdza czy zawodnik nie jest w jakimœ innym teamie
---Transfer z klubu a do klubu b
---Zakoñcz mecz -> zmiana wyników w tabeli Match
---Zakup bilet - przypisujemy do fana, trzeba sprawdzic czy mecz sie juz nie zakonczyl, czy mecz istnieje oraz czy sa jakies wolne bilety?
---Przy transferze sprawdzamy czy druzyna nie ma maxa zawodnikow - 15
---Dodaj druzyne -> Sprawdzamy czy jest mniej niz 15 druzyn
---Usun dru¿yne
---RANKING -> 1 MIEJSCE - NAJLEPSZY WSPOLCZYNNIK ZWYCIEST/PORAZEK - POTEM WSPOLCZYNNIK ZDOBYTE/STRACONE PKT
---FUNKCJa wyswietlajaca ranking
---DODAJ SPONSORA
---DODAJ STATYSTYKI DLA GRACZA W MECZU -> weryfikacja czy dany zawodnik gral w danym meczu
---Dodaj mecz
---Usun mecz
---Koniec kontraktu u zawodnika - usuwamy mu druzyne
---Dodaj fana
-
---MO¯E:
---FUNKCJA KTÓRA POBIERA WSZYSTKIE MECZE DLA DANEJ DRU¯YNY
---FUNKCJA KTÓRA ZWRACA DANE O MECZU POMIEDZY DRUZYNAMI X/Y
---FUNKCJA KTÓRA ZWRACA WSZYSTKIE MECZE Z DANEJ DATY
---STATYSTYKI -> SUMA ZAGRANYCH MINUT WSZYSTKICH ZAWODNIKÓW Z DANEJ DRUZYNY MUSI BYC ROWNA 300 MINUT (5x 1H)
---
-
 
 
 
